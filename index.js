e49046cd66c79a09f36f848d770c1e70c7f69fa2
lynxxxz <mamattzyku11@gmail.com>
2025-05-27 02:18:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'X3:Adrianz\n' // Nama kamu
+            + 'ORG:lynxxz;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6287764807759:+62 877-6480-7759\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
